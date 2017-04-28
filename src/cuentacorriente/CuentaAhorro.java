4a120f60b8e2d12bcdc099ee7d07db63ff0236fd
Alumno <Alumno@INF716>
2017-04-28 14:00:53 +0200

    Añadimos ficheros de configuración

--- src/cuentacorriente/CuentaAhorro.java
+++ src/cuentacorriente/CuentaAhorro.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cuentacorriente;
 
-/**
- *
- * @author Alumno
- */
-public class CuentaAhorro {
-    
+public class CuentaAhorro extends CuentaCorriente {
+
+    double interes;
+
+    public CuentaAhorro(String nombre, String numCuenta, double saldo, double interes) {
+        super(nombre, numCuenta, saldo, 0);
+        this.interes = interes;
+    }
+
+    public CuentaAhorro(String nombre, String numCuenta, double interes) {     
+        this.interes = interes;
+    }
+
+    public double getInteres() {
+        return this.interes;
+    }
 }
